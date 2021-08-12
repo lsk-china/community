77de4ec496f9b3d1e17182b9269c7b139613e72b
15904323117 <2075929352@qq.com>
2021-08-12 19:33:20 +0800

    backend

--- back/common/common-util/src/main/java/com/lsk/community/back/common/utils/CloudUtil.java
+++ back/common/common-util/src/main/java/com/lsk/community/back/common/utils/CloudUtil.java
@@ -1,5 +1,21 @@
 package com.lsk.community.back.common.utils;
 
-public class CloudUtil {
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
+public class CloudUtil {
+	private static LoadBalancerClient loadBalancerClient = SpringUtil.getBean(LoadBalancerClient.class);
+	private static RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
+
+	private static String getServiceRootURL(String serviceName){
+		ServiceInstance serviceInstance = loadBalancerClient.choose(serviceName);
+		return "http://"+serviceInstance.getHost()+":"+serviceInstance.getPort();
+	}
+
+	public static <T> T requestService(String service, String api, Map<String, Object> params, Class<T> type) {
+		
+	}
 }
