1433ea708c6bb58b9c39c7dbac32ea269f0722d2
15904323117 <2075929352@qq.com>
2021-08-16 16:06:41 +0800

    Backend

--- back/auth/src/main/java/com/lsk/community/back/auth/controller/Controller.java
+++ back/auth/src/main/java/com/lsk/community/back/auth/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.lsk.community.back.auth.controller;
 
 import com.lsk.community.back.auth.authc.Authc;
+import com.lsk.community.back.common.authz.aspect.annotation.RequireRequestKey;
 import com.lsk.community.back.common.response.aspect.annotation.JsonReturn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -25,18 +26,19 @@ public class Controller {
 	}
 
 	@JsonReturn
+	@RequireRequestKey
 	@PostMapping("/login")
 	public Object login(@CookieValue("token") String token, String identity, String password) {
 		authc.login(token, identity, password);
 		return new Object();  // 实际会返回{code: 0, data:{}, message: "Success"}
 	}
-	@JsonReturn
-	@PostMapping("/loginWithNewToken")
-	public Object loginWithNewToken(HttpServletResponse resp, String identity, String password) {
-		String token = authc.loginWithNewToken(identity, password);
-		Cookie tokenCookie = new Cookie("token", token);
-		tokenCookie.setMaxAge(86400);
-		resp.addCookie(tokenCookie);
-		return new Object();
-	}
+//	@JsonReturn
+//	@PostMapping("/loginWithNewToken")
+//	public Object loginWithNewToken(HttpServletResponse resp, String identity, String password) {
+//		String token = authc.loginWithNewToken(identity, password);
+//		Cookie tokenCookie = new Cookie("token", token);
+//		tokenCookie.setMaxAge(86400);
+//		resp.addCookie(tokenCookie);
+//		return new Object();
+//	}
 }
